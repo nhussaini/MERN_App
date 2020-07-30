9213fc486a3ff3bc13923c6785ced0c61c7718af
unknown <nhussaini@luc.edu>
2020-07-30 16:05:04 -0700

    got the current user profile

--- models/Profile.js
+++ models/Profile.js
@@ -112,4 +112,4 @@ const ProfileSchema = new mongoose.Schema({
 
 })
 
-module.exports = Profile = mogoose.model('profile',ProfileSchema);
+module.exports = Profile = mongoose.model('profile',ProfileSchema);
