fe117175e6ab78b1967ecc07c0d6fafaad2ddc90
unknown <nhussaini@luc.edu>
2020-07-29 17:46:50 -0700

    implemented JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar'); 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User =  require('../../models/User');
@@ -47,10 +49,23 @@ router.post('/', [
 
     user.password =  await bcrypt.hash(password,salt);
 
+    //Store the user in database
     await user.save();
 
-    //return jsonwebtoken
-    res.send('User registered');
+     //return jsonwebtoken
+    const payload = {
+        user : {
+            id: user.id
+        }
+    }
+    jwt.sign(payload, config.get('jwtSecret'),
+    {expiresIn: 360000},
+    (err, token)=>{
+        if(err) throw err;
+        res.json({token}); 
+    });
+
+   
 
     }catch(err){
         console.error(err.message);
